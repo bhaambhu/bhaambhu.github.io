bdc703f6ea7d574110192c106112e8ff8f6e5915
Sanjay Bhaambhu <bhaambhu@gmail.com>
2022-11-24 22:28:32 +0530

    Added first build

---
+++ out/_next/static/chunks/pages/old-74ca0b6bb81bcd94.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[186],{2770:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/old",function(){return c(2784)}])},2784:function(c,b,a){"use strict";a.r(b);var d=a(5893);a(5675),a(7294),b.default=function(){return(0,d.jsx)("div",{})}}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=2770)}),_N_E=a.O()}])
