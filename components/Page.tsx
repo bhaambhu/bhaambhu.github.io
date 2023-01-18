3ea465b8e2afb562df76562d16dd89e75694bddd
Sanjay Bhaambhu <bhaambhu@gmail.com>
2023-01-18 11:28:36 +0530

    fixed margins

--- components/Page.tsx
+++ components/Page.tsx
@@ -9,7 +9,7 @@ export default function Page(pageProps: PageProps) {
     <div className='bg-slate-50'>
       <SEO pageTitle={pageProps.title} keywords={pageProps.seoKeywords} pageDesc={pageProps.description} />
       <Navbar title={pageProps.title} links={pageProps.links} />
-      <main className={twMerge('print:p-0 print:m-0 p-5 xsm:px-12 md:px-24 lg:px-44 max-w-7xl m-auto'+pageProps.className)}>
+      <main className={twMerge(' p-5 xsm:px-12 md:px-24 lg:px-44 max-w-7xl m-auto w-full print:p-0 print:m-0 '+pageProps.className)}>
         {pageProps.children}
       </main>
     </div >
